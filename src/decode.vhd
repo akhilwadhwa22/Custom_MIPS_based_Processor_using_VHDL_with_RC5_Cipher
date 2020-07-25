cb02b8144a2f0d085d3748674cfa3e4fd5e3cc86
Akhil Wadhwa <44124142+akhilwadhwa22@users.noreply.github.com>
2020-07-25 17:19:18 -0400

    Update decode.vhd

--- src/decode.vhd
+++ src/decode.vhd
@@ -17,38 +17,36 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity DecodeUnit is
-    Port ( opcode : in STD_LOGIC_VECTOR(5 downto 0);
-		   func : in STD_LOGIC_VECTOR(5 downto 0);
-           MemtoReg : out  STD_LOGIC;
-           MemWrite : out  STD_LOGIC;
-           Branch : out  STD_LOGIC;
-           ALUControl : out  STD_LOGIC_VECTOR(3 downto 0);
-	       ALUsrc : out  STD_LOGIC;
-           RegDst : out  STD_LOGIC;
-           RegWrite : out  STD_LOGIC;
-           jmp      :     out  STD_LOGIC;
-           halt: out STD_LOGIC
+    Port ( opcode  	: in STD_LOGIC_VECTOR(5 downto 0);
+	   func 	: in STD_LOGIC_VECTOR(5 downto 0);
+           MemtoReg 	: out  STD_LOGIC;
+           MemWrite 	: out  STD_LOGIC;
+           Branch 	: out  STD_LOGIC;
+           ALUControl 	: out  STD_LOGIC_VECTOR(3 downto 0);
+           ALUsrc 	: out  STD_LOGIC;
+           RegDst 	: out  STD_LOGIC;
+           RegWrite 	: out  STD_LOGIC;
+           jmp      	: out  STD_LOGIC;
+           halt		: out STD_LOGIC
            );
 end DecodeUnit;
 
 architecture Behavioral of DecodeUnit is
 
-SIGNAL MemtoRegS: STD_LOGIC := '0';
-SIGNAL MemWriteS: STD_LOGIC := '0';
-SIGNAL BranchS:  STD_LOGIC := '0';
-SIGNAL ALUControlS: STD_LOGIC_VECTOR(3 downto 0);
-SIGNAL ALUsrcS: STD_LOGIC := '0';
-SIGNAL RegDstS: STD_LOGIC := '0';
-SIGNAL RegWriteS: STD_LOGIC := '0';
-SIGNAL jmpS: STD_LOGIC := '0';
-SIGNAL halts: STD_LOGIC:='0';
+SIGNAL MemtoRegS: STD_LOGIC 	:= '0';
+SIGNAL MemWriteS: STD_LOGIC 	:= '0';
+SIGNAL BranchS:  STD_LOGIC 	:= '0';
+SIGNAL ALUControlS		: STD_LOGIC_VECTOR(3 downto 0);
+SIGNAL ALUsrcS: STD_LOGIC 	:= '0';
+SIGNAL RegDstS: STD_LOGIC 	:= '0';
+SIGNAL RegWriteS: STD_LOGIC 	:= '0';
+SIGNAL jmpS: STD_LOGIC 		:= '0';
+SIGNAL halts: STD_LOGIC		:='0';
 
 begin
 Process(opcode, func)
 BEGIN
-IF(opcode = "000000")	THEN
-
-	
+IF(opcode = "000000")	THEN	
 	ALUSrcS <= '0';
 	MemtoRegS <= '0';
 	MemWriteS <= '0';
@@ -57,6 +55,7 @@ IF(opcode = "000000")	THEN
 	RegDstS <= '1';
 	jmpS <='0';
 	halts<='0';
+
 	IF(func = "010010") THEN
 		ALUControlS <= "0000";     --AND
 	ELSIF(func = "010011") THEN
@@ -70,134 +69,120 @@ IF(opcode = "000000")	THEN
 	ELSIF(func = "010101") THEN
 		ALUControlS <= "0101";     ---LEFT ROTATE -ADD
 	ELSIF(func = "010110") THEN
-		ALUControlS <= "0110";     ---SUB-RIGHT
-		
+		ALUControlS <= "0110";     ---SUB-RIGHT		
 	END IF;
 ELSE      -------   RegDstS is '1' when Instruction is I type '0' otherwise
 	IF(opcode = "000011") THEN
-		ALUControlS <= "0000";   ----ANDI
-		RegDstS <= '0';
-		ALUSrcS <= '1';
-
-		MemtoRegS <= '0';
-		MemWriteS <= '0';
-		BranchS <= '0';
-		
-		RegWriteS <= '1';
-		jmpS <='0';
-		halts<='0';
+		ALUControlS 	<= "0000";   ----ANDI
+		RegDstS 	<= '0';
+		ALUSrcS 	<= '1';
+		MemtoRegS 	<= '0';
+		MemWriteS 	<= '0';
+		BranchS 	<= '0';
+		RegWriteS	<= '1';
+		jmpS 		<= '0';
+		halts		<= '0';
 
 	ELSIF(opcode = "000100") THEN
-		ALUControlS <= "0001";     ----ORI
-		RegDstS <= '0';
-		ALUSrcS <= '1';
-
-		MemtoRegS <= '0';
-		MemWriteS <= '0';
-		BranchS <= '0';
-		
-		RegWriteS <= '1';
-		jmpS <='0';
-		halts<='0';
+		ALUControlS 	<= "0001";     ----ORI
+		RegDstS 	<= '0';
+		ALUSrcS 	<= '1';
+		MemtoRegS 	<= '0';
+		MemWriteS 	<= '0';
+		BranchS 	<= '0';
+		RegWriteS 	<= '1';
+		jmpS 		<= '0';
+		halts		<= '0';
 
 	ELSIF(opcode = "000111") THEN
-		ALUControlS <= "0111";    ---LW
-		RegDstS <= '0';
-		RegWriteS <= '1';
-     	ALUSrcS <= '1';
-     	MemtoRegS <= '1';
-
-     	BranchS <= '0';
-     	MemWriteS <= '0';
-     	jmpS <='0';
-     	halts<='0';
+		ALUControlS 	<= "0111";    ---LW
+		RegDstS 	<= '0';
+		RegWriteS 	<= '1';
+     		ALUSrcS 	<= '1';
+     		MemtoRegS 	<= '1';
+     		BranchS		<= '0';
+     		MemWriteS	<= '0';
+     		jmpS  		<= '0';
+     		halts		<= '0';
 
 	ELSIF(opcode = "001000") THEN
-		ALUControlS <= "0111";    ----SW
-		RegDstS <= '0';
-        MemWriteS<= '1';
-		ALUSrcS <= '1';
-
-		MemtoRegS <= '0';
-		BranchS <= '0';
-		RegWriteS <= '0';
-		jmpS <='0';
-		halts<='0';
+		ALUControlS 	<= "0111";    ----SW
+		RegDstS 	<= '0';
+        	MemWriteS	<= '1';
+		ALUSrcS 	<= '1';
+		MemtoRegS 	<= '0';
+		BranchS 	<= '0';
+		RegWriteS 	<= '0';
+		jmpS 		<= '0';
+		halts		<= '0';
 
 	ELSIF(opcode = "001001") THEN
-		ALUControlS <= "1000";    ----BLT
-		RegDstS <= '0';
-		BranchS<='1';
-
-		MemtoRegS <= '0';
-		MemWriteS <= '0';
-		RegWriteS <= '0';
-		ALUSrcS <= '0';
-		jmpS <='0';
-        halts<='0';
+		ALUControlS 	<= "1000";    ----BLT
+		RegDstS 	<= '0';
+		BranchS		<= '1';
+		MemtoRegS 	<= '0';
+		MemWriteS 	<= '0';
+		RegWriteS 	<= '0';
+		ALUSrcS 	<= '0';
+		jmpS 		<= '0';
+        	halts		<= '0';
 
 	ELSIF(opcode = "001010") THEN
-		ALUControlS <= "1001";    ---BEQ
-		RegDstS <= '0';
-		BranchS<='1';
-
-        halts<='0';
-		MemtoRegS <= '0';
-		MemWriteS <= '0';
-		RegWriteS <= '0';
-		ALUSrcS <= '0';
-		jmpS <='0';
+		ALUControlS 	<= "1001";    ---BEQ
+		RegDstS 	<= '0';
+		BranchS		<= '1';
+        	halts   	<= '0';
+		MemtoRegS 	<= '0';
+		MemWriteS 	<= '0';
+		RegWriteS 	<= '0';
+		ALUSrcS 	<= '0';
+		jmpS 		<= '0';
 
 	ELSIF(opcode = "001011") THEN
-		ALUControlS <= "1010";    ----BNE
-		RegDstS <= '0';
-		BranchS<='1';
-
-		MemtoRegS <= '0';
-		MemWriteS <= '0';
-		RegWriteS <= '0';
-		ALUSrcS <= '0';
-		jmpS <='0';
-		halts<='0';
+		ALUControlS 	<= "1010";    ----BNE
+		RegDstS 	<= '0';
+		BranchS		<= '1';
+		MemtoRegS 	<= '0';
+		MemWriteS 	<= '0';
+		RegWriteS 	<= '0';
+		ALUSrcS 	<= '0';
+		jmpS 		<= '0';
+		halts		<= '0';
 		
 	ELSIF(opcode = "001100") THEN
-	    jmpS <='1';
-	    ALUControlS <= "1110";   -- JMP
-		
-		RegDstS <= '0';
-		BranchS<='0';
-		MemtoRegS <= '0';
-		MemWriteS <= '0';
-		RegWriteS <= '0';
-		ALUSrcS <= '1';
-		haltS<='0';
+	    	jmpS 		<= '1';
+	    	ALUControlS 	<= "1110";   -- JMP
+		RegDstS 	<= '0';
+		BranchS		<= '0';
+		MemtoRegS 	<= '0';
+		MemWriteS 	<= '0';
+		RegWriteS 	<= '0';
+		ALUSrcS 	<= '1';
+		haltS		<= '0';
 		
 	ELSIF(opcode = "111111") THEN
-	   ALUControlS <= "1111"; -- HLT
-	   	haltS<='1';
-	   	RegDstS <= '0';
-		BranchS<='0';
-		MemtoRegS <= '0';
-		MemWriteS <= '0';
-		RegWriteS <= '0';
-		ALUSrcS <= '0';
-		jmpS <='0';
-		
-	   	
-
+	   	ALUControlS 	<= "1111"; -- HLT
+	   	haltS		<= '1';
+	   	RegDstS 	<= '0';
+		BranchS		<= '0';
+		MemtoRegS 	<= '0';
+		MemWriteS 	<= '0';
+		RegWriteS 	<= '0';
+		ALUSrcS 	<= '0';
+		jmpS 		<= '0';
 	END IF;
 END IF;
 end process;
 
-MemtoReg <= MemtoRegS;
-MemWrite <= MemWriteS;
-Branch <= BranchS;
-ALUControl <= ALUControlS;
-ALUsrc <= ALUsrcS;
-RegDst <= RegDstS;
-RegWrite <= RegWriteS;
-jmp<=jmpS;
-halt<=haltS;
+MemtoReg 	<= MemtoRegS;
+MemWrite 	<= MemWriteS;
+Branch 		<= BranchS;
+ALUControl 	<= ALUControlS;
+ALUsrc 		<= ALUsrcS;
+RegDst		<= RegDstS;
+RegWrite 	<= RegWriteS;
+jmp		<=jmpS;
+halt		<=haltS;
 
 end Behavioral;
 
